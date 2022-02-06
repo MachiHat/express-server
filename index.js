c126b675995f7b5ed05a88f2d3b55d2fd22398b4
MachiHat <mati01herrera@gmail.com>
2022-02-06 11:57:31 -0300

    error: unexpected token in Json pos 1

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const express = require('express');
-
+const express = require("express");
 const PORT = process.env.PORT || 8080;
-
 const app = express();
 
-app.get('/api', (req, res) => {
-    res.json({message: "HELLO WORLD, THIS IS EXPRESS"})
-})
+const fs = require("fs");
+const fsPromises = require("fs/promises");
+class Container {
+  constructor(id, title, author, price, thumbnail) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.price = price;
+    this.thumbnail = thumbnail;
+  }
 
-const connectedServer = app.listen(PORT, () => {
-    console.log(`SERVER IS LIVE ON PORT: ${[PORT]}`)
+  static getProducts() {
+    const getter = async () => {
+      try {
+        const data = fsPromises.readFile("./products.json", "utf-8");
+        const localBooks = JSON.parse(data);
+        return localBooks;
+      } catch (error) {
+        throw error;
+      }
+    };
+    return getter();
+  }
+}
+
+app.get("/products", (req, res) => {
+  res.json({ products: Container.getProducts() });
 });
 
-connectedServer.on('error', (error) => {
-    console.log(error.message);
-})
+const connectedServer = app.listen(PORT, () => {
+  console.log(`SERVER IS LIVE ON PORT: ${[PORT]}`);
+});
+
+connectedServer.on("error", (error) => {
+  console.log(error.message);
+});

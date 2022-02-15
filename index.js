ee234658a00f6b8c11ce6311da71004407c42f5b
MachiHat <mati01herrera@gmail.com>
2022-02-14 21:26:46 -0300

    entrega 8

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
 const express = require("express");
-const PORT = process.env.PORT || 8080;
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-const fs = require("fs");
-const fsPromises = require("fs/promises");
-class Container {
-  constructor(id, title, author, price, thumbnail) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.price = price;
-    this.thumbnail = thumbnail;
-  }
+const apiRoutes = require('./routers/index');
 
-  static getProducts() {
-    const getter = async () => {
-      try {
-        const data = fsPromises.readFile("./products.json", "utf-8");
-        const localBooks = JSON.parse(data);
-        return localBooks;
-      } catch (error) {
-        throw error;
-      }
-    };
-    return getter();
-  }
-}
+// MIDDLEWARES
 
-app.get("/products", (req, res) => {
-  res.json({ products: Container.getProducts() });
-});
+app.use(express.static('public'));
+
+// ROUTES
+
+app.use('/api', apiRoutes);
 
 const connectedServer = app.listen(PORT, () => {
   console.log(`SERVER IS LIVE ON PORT: ${[PORT]}`);
